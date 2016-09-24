0a1b7592f2ca0261bd2be81d048c0b68b7ddfa10
chaduhduh <chadd.myers3@gmail.com>
2016-09-24 15:40:33 -0700

    adds oauth

--- helloworld_api.py
+++ helloworld_api.py
@@ -1,60 +1,89 @@
 """Hello World API implemented using Google Cloud Endpoints.
 
-Contains declarations of endpoint, endpoint methods,
-as well as the ProtoRPC message class and container required
-for endpoint method definition.
+Defined here are the ProtoRPC messages needed to define Schemas for methods
+as well as those methods defined in an API.
 """
+
+
 import endpoints
-from protorpc import messages
+from protorpc import messages   
 from protorpc import message_types
 from protorpc import remote
 
 
-# If the request contains path or querystring arguments,
-# you cannot use a simple Message class.
-# Instead, you must use a ResourceContainer class
-REQUEST_CONTAINER = endpoints.ResourceContainer(
-    message_types.VoidMessage,
-    name=messages.StringField(1),
-)
-
-GREET_CONTAINER = endpoints.ResourceContainer(
-    message_types.VoidMessage,
-    name=messages.StringField(1),
-    period=messages.StringField(2)
-)
+# TODO: Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '263680208865-0sifgjv3gtuhe41cmn3uvp8b47993v71.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'replace this with your Android client ID'
+IOS_CLIENT_ID = 'replace this with your iOS client ID'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
 
 
 package = 'Hello'
 
 
-class Hello(messages.Message):
-    """String that stores a message."""
-    greeting = messages.StringField(1)
-    period = messages.StringField(2)
+class Greeting(messages.Message):
+    """Greeting that stores a message."""
+    message = messages.StringField(1)
 
 
-@endpoints.api(name='helloworldendpoints', version='v1')
+class GreetingCollection(messages.Message):
+    """Collection of Greetings."""
+    items = messages.MessageField(Greeting, 1, repeated=True)
+
+
+STORED_GREETINGS = GreetingCollection(items=[
+    Greeting(message='hello world!'),
+    Greeting(message='goodbye world!'),
+])
+
+
+@endpoints.api(name='helloworld', version='v1',
+               allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
+                                   IOS_CLIENT_ID],
+               audiences=[ANDROID_AUDIENCE])
 class HelloWorldApi(remote.Service):
     """Helloworld API v1."""
 
-    @endpoints.method(message_types.VoidMessage, Hello,
-      path = "sayHello", http_method='GET', name = "sayHello")
-    def say_hello(self, request):
-      return Hello(greeting="Hello World")
+    MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(
+            Greeting,
+            times=messages.IntegerField(2, variant=messages.Variant.INT32,
+                                        required=True))
 
-    @endpoints.method(REQUEST_CONTAINER, Hello,
-      path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
-    def say_hello_by_name(self, request):
-      greet = "Hello {}".format(request.name)
-      return Hello(greeting=greet)
+    @endpoints.method(MULTIPLY_METHOD_RESOURCE, Greeting,
+                      path='hellogreeting/{times}', http_method='POST',
+                      name='greetings.multiply')
+    def greetings_multiply(self, request):
+        return Greeting(message=request.message * request.times)
 
-    @endpoints.method(GREET_CONTAINER, Hello,
-      path = "greetByPeriod", http_method='GET', name = "greetByPeriod")
-    def greet_by_period(self, request):
-      greet = "Hello {}".format(request.name)
-      period = request.period;
-      return Hello(greeting=greet, period=period)
+    @endpoints.method(message_types.VoidMessage, GreetingCollection,
+                      path='hellogreeting', http_method='GET',
+                      name='greetings.listGreeting')
+    def greetings_list(self, unused_request):
+        return STORED_GREETINGS
+
+    ID_RESOURCE = endpoints.ResourceContainer(
+            message_types.VoidMessage,
+            id=messages.IntegerField(1, variant=messages.Variant.INT32))
+
+    @endpoints.method(ID_RESOURCE, Greeting,
+                      path='hellogreeting/{id}', http_method='GET',
+                      name='greetings.getGreeting')
+    def greeting_get(self, request):
+        try:
+            return STORED_GREETINGS.items[request.id]
+        except (IndexError, TypeError):
+            raise endpoints.NotFoundException('Greeting %s not found.' %
+                                              (request.id,))
+
+    @endpoints.method(message_types.VoidMessage, Greeting,
+                      path='hellogreeting/authed', http_method='POST',
+                      name='greetings.authed')
+    def greeting_authed(self, request):
+        current_user = endpoints.get_current_user()
+        email = (current_user.email() if current_user is not None
+                 else 'Anonymous')
+        return Greeting(message='hello %s' % (email,))
 
 
 APPLICATION = endpoints.api_server([HelloWorldApi])
